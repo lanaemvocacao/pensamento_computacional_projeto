5f74cfc5df2233e8e902379778fbc51391074a3d
lanaemvocacao <lanag2202@gmail.com>
2026-03-09 16:31:22 -0300

    Add files via upload

---
+++ simplificacao_if&elif&else.py
@@ -0,0 +1,20 @@
+'''
+
+
+'''
+
+preço_frutas = {
+    'maça': 2.5,
+    'banana': 1.8,
+    'laranja': 3.0
+}
+
+fruta_desejada = 'maça'
+
+resultado = preço_frutas.get(fruta_desejada, 'fruta não encontrada')
+
+print (f"O preço da {fruta_desejada} é R$ {resultado}")
+
+
+
+ 
---
+++ teste_idade.py
@@ -0,0 +1,19 @@
+'''
+
+
+'''
+
+print('Digite a idade do jovem: ')
+
+idade = int(input())
+if idade < 18:
+    print('Jovem é menor de idade')
+
+elif idade <= 65:
+    print('Jovem é adulto de idade')
+
+
+
+
+else:
+    print('Jovem é idoso de idade')
